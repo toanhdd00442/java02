5c1ff82241637d8e5c1eeb17b4dca3499d9d2e0a
Hoang Dinh Toan <toanhdd00442@fpt.edu.vn>
2017-08-26 10:06:17 -0700

    Upload project.

---
+++ src/exception/Exception.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exception;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Exception {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String names="Truong,Nam,Khanh,Danh";
+        String[] arrayName = names.split(",");
+        for (int i=0;i< arrayName.length;i++){
+            System.out.println(i+1+".");
+            System.out.println(arrayName[i]);
+        }
+                
+      
+    }
+    
+            
+}
